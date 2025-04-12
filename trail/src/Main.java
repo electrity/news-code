5fb24c92c43c8ea2e776b163b0d808e6f6d2542a
24425 <2442599220@qq.com>
2025-04-12 20:58:23 +0800

    git add . git commit -m "day1" git push origin main

--- trail/src/Main.java
+++ trail/src/Main.java
@@ -1,11 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main{
     public static void main(String[] args){
         int i,j;
-        String arg = "";
+        StringBuilder arg;
         for(i = 0;i < 5;i++){
-            arg = "";
+            arg = new StringBuilder();
             for(j = 0;j < i + 1;j++){
-                arg += "*";
+                arg.append("*");
             }
             System.out.println(arg);
         }
@@ -14,42 +17,39 @@ public class Main{
         int[] scores = {1,2,100,100,2};
         System.out.println(scores100(scores));
         System.out.println(scoresIncreasing(scores));
+        List<Integer> list = new ArrayList<>();
+        list.add(0);
+        list.add(9);
+        list.add(3);
+        list.add(0);
+        list.add(8);
+        list.add(null);
+
+        System.out.println(list);
     }
     public static void printIndexed(String str){
-        int Slength = str.length();
-        String output = "";
-        for(int i = 0;i < Slength;i++){
+        int Length = str.length();
+        StringBuilder output = new StringBuilder();
+        for(int i = 0;i < Length;i++){
             if(str.charAt(i) != ' ') {
-                output += str.charAt(i);
-                output += Slength - i - 1;
-                output += ' ';
+                output.append(str.charAt(i));
+                output.append(Length - i - 1);
+                output.append(' ');
             }
         }
         System.out.println(output);
     }
     public static String stutter(String str){
-        String output = "";
+        StringBuilder output = new StringBuilder();
         for(int i = 0;i < str.length();i++){
             if(str.charAt(i) != ' ') {
-                output += str.charAt(i);
-                output += str.charAt(i);
+                output.append(str.charAt(i));
+                output.append(str.charAt(i));
             }else{
-                output += ' ';
+                output.append(' ');
             }
         }
-        return output;
-    }
-    public static int quatrant(int x,int y){
-        if (x == 0 || y == 0)
-            return 0;
-        else if(y > 0 && x > 0)
-            return 1;
-        else if(y > 0 && x < 0)
-            return 2;
-        else if(y < 0 && x < 0)
-            return 3;
-        else
-            return 4;
+        return output.toString();
     }
     public static boolean scoresIncreasing(int[] scores) {
         int index = 0;
