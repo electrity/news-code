19b13354e0330980cac97b33875ac5ba387a886f
24425 <2442599220@qq.com>
2025-04-29 20:04:21 +0800

    git add. git commit -a git push

--- hw0b/src/MapExercises.java
+++ hw0b/src/MapExercises.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -7,7 +8,15 @@ public class MapExercises {
      */
     public static Map<Character, Integer> letterToNum() {
         // TODO: Fill in this function.
-        return null;
+        Map<Character, Integer> letterMap = new HashMap<>();
+        // 遍历小写字母 'a' 到 'z'
+        for (char c = 'a'; c <= 'z'; c++) {
+            // 计算字母对应的数字
+            int num = c - 'a' + 1;
+            // 将字母和对应的数字放入映射中
+            letterMap.put(c, num);
+        }
+        return letterMap;
     }
 
     /** Returns a map from the integers in the list to their squares. For example, if the input list
@@ -15,12 +24,21 @@ public class MapExercises {
      */
     public static Map<Integer, Integer> squares(List<Integer> nums) {
         // TODO: Fill in this function.
-        return null;
+        Map<Integer,Integer> intMap = new HashMap<>();
+        for(int i : nums){
+            intMap.put(i,i*i);
+        }
+        return intMap;
     }
 
     /** Returns a map of the counts of all words that appear in a list of words. */
     public static Map<String, Integer> countWords(List<String> words) {
         // TODO: Fill in this function.
+        Map<String,Integer> letterMap = new HashMap<>();
+        for(String word : words){
+            if(letterMap.containsKey(word)) letterMap.put(word,letterMap.get(word) + 1);
+            else letterMap.put(word,1);
+        }
         return null;
     }
 }
